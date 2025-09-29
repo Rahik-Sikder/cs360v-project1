d9338430f3cda9a130e4f5ec7ec848b47d4e6a6e
Rahik-Sikder <sikder.rahik@gmail.com>
2025-09-29 12:51:01 -0500

    fxied sched_yield bug

--- kern/sched.c
+++ kern/sched.c
@@ -68,14 +68,15 @@ sched_yield(void)
         /* Your code here */
 	#ifndef VMM_GUEST
 		if(curenv->env_type == ENV_TYPE_GUEST) {
+			if(curenv->env_vmxinfo.vcpunum != cpunum())
+				return;
 			int res = vmxon();
 			if (res < 0){
 				env_destroy(curenv);
 			}
-		} else {
-			env_run(curenv);
 		}
 	#endif
+		env_run(curenv);		
 
 	}
 
